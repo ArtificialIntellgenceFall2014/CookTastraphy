36d0163d90be09b57dc7f68145b49e0485e77ad8
CharleneCoffman <charlene.coffman@arch-di.com>
2014-11-14 12:18:53 -0600

    more changes

--- CookTastraphy/src/restraunt/Pantry.java
+++ CookTastraphy/src/restraunt/Pantry.java
@@ -11,5 +11,13 @@ public class Pantry {
 		allItems[1] = sauce;
 		Item rsauce = new Item("Red Sauce", 10);
 		allItems[2] = rsauce;
+		Item dough = new Item("Dough", 10);
+		allItems[3] = dough;
+		Item meatballs = new Item("Meatballs", 10);
+		allItems[4] = meatballs;
+		Item chicken = new Item("Chicken", 10);
+		allItems[5] = chicken;
+		Item cheese = new Item("Cheese", 10);
+		allItems[6] = cheese;
 	}
 }
