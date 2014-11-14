06112c11a1f03b8b202ad8381b3b92301975f464
CharleneCoffman <charlene.coffman@arch-di.com>
2014-11-14 12:34:23 -0600

    more things

--- CookTastraphy/src/graphics/RestrauntGameBoard.java
+++ CookTastraphy/src/graphics/RestrauntGameBoard.java
@@ -1,5 +1,20 @@
 package graphics;
+import java.awt.*;
+import javax.swing.*;
 
 public class RestrauntGameBoard {
-
+	public JFrame frame;
+	
+	public RestrauntGameBoard(){
+		
+		JPanel p = new JPanel(new BorderLayout()); //PREFERRED!
+		JButton okButton = new JButton("OK");
+	    p.add(okButton);
+	    JButton cancelButton = new JButton("Cancel");
+	    p.add(cancelButton);
+	    JFrame frame = new JFrame("Oval Sample");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.add(p);
+	    frame.setSize(300, 200);
+	}
 }
--- CookTastraphy/src/restraunt/Restraunt.java
+++ CookTastraphy/src/restraunt/Restraunt.java
@@ -1,10 +1,15 @@
 package restraunt;
+import graphics.*;
 
 public class Restraunt {
 	//initialize panty, chef, runner, menu
 	//create and display gameboard
 	//access orders (automatic or actual input?
 	
-	
+	public static void main(String [ ] args)
+	{
+	    RestrauntGameBoard theBoard = new RestrauntGameBoard();
+	    theBoard.frame.setVisible(true);
+	}
 	
 }
