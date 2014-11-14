d84a9b37fce55940682bb837f2d52b75ebc085aa
CharleneCoffman <charlene.coffman@arch-di.com>
2014-11-14 12:13:46 -0600

    More things are changing

--- CookTastraphy/src/restraunt/Pantry.java
+++ CookTastraphy/src/restraunt/Pantry.java
@@ -3,4 +3,13 @@ package restraunt;
 public class Pantry {
 
 	public Item[] allItems;
+	
+	public Pantry(){
+		Item noodles = new Item("Noodles", 10);
+		allItems[0] = noodles;
+		Item sauce = new Item("White Sauce", 10);
+		allItems[1] = sauce;
+		Item rsauce = new Item("Red Sauce", 10);
+		allItems[2] = rsauce;
+	}
 }
